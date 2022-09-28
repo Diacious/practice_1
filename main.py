ce87f1102379fe8755b13484fd5eaf0ffa73f114
Dima <votchinnikov.dima@mail.ru>
2022-09-28 12:23:15 +0300

    edit python file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+new_line = input('u may add some new lines in your data if you wish')
+
 with f as open('data.txt'):
 	data = [line.split(',') for line in f]
 	for row in data:
-		print(f'name: {data[0]}, surname: {data[1]}, age: {data[3]}, salary: {data[-1]}'
-		
+		print(f'name: {data[0]}, surname: {data[1]}, age: {data[3]}, salary: {data[-1]}')
+	f.write(data + new_line)
+
+	
