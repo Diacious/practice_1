4b50c230e3a98974bde5f107f0a9e45df75734f8
Dima <votchinnikov.dima@mail.ru>
2022-09-30 12:25:22 +0300

    reimplement main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-new_line = input('u may add some new lines in your data if you wish')
+func = lambda x, y: func(x + [y], input('write lines, to stop write: "stop" ')) if y != 'stop' else x
+new_lines = func([], input('write lines, to stop write: "stop" '))
 
-with open('data.txt', 'r+') as f:
-	data = [line.split(',') for line in f]
-
-	for row in data:
-		print(f'name: {row[0]}, surname: {row[1]}, age: {row[2]}, salary: {row[-1]}')
-
-	if new_line:
-		f.write('\n' + new_line)
-
-	
+print(new_lines)
+if new_lines:
+	with open('data.txt', 'w') as f:
+		for line in new_lines:
+			f.write(line + '\n')
+else:
+	print('data was preserved')
