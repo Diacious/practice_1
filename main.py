f3d7a406787638af8a1c0afb60af2fcfddc22982
Dima <votchinnikov.dima@mail.ru>
2022-09-30 08:57:26 +0300

    fixed some error

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 new_line = input('u may add some new lines in your data if you wish')
 
-with f as open('data.txt'):
+with open('data.txt', 'r+') as f:
 	data = [line.split(',') for line in f]
+
 	for row in data:
-		print(f'name: {data[0]}, surname: {data[1]}, age: {data[3]}, salary: {data[-1]}')
-	f.write(data + new_line)
+		print(f'name: {row[0]}, surname: {row[1]}, age: {row[2]}, salary: {row[-1]}')
+
+	if new_line:
+		f.write('\n' + new_line)
 
 	
